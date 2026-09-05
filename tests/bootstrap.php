97a6ffaa4fa35c036e3d8bd9df5454221510756e
Simplix Innovations <136711735+SimplixInnovationsAdmin@users.noreply.github.com>
2026-09-05 21:18:13 +0400

    Q16: load migration core test fixture

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,6 @@ require __DIR__ . '/support/wordpress-availability.php';
 require __DIR__ . '/support/wordpress-gateway-settings.php';
 require __DIR__ . '/support/wordpress-public-order-status.php';
 require __DIR__ . '/support/wordpress-migration-bootstrap.php';
-require __DIR__ . '/support/wordpress-migration-admin.php';
+require __DIR__ . '/support/wordpress-migration-admin.php';\nrequire __DIR__ . '/support/wordpress-migration-core.php';
 require __DIR__ . '/support/wordpress-subscription-presentation.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
