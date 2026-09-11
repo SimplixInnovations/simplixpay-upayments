0b330d58149ce20469080a586aee13aa9891e946
Simplix Innovations <136711735+SimplixInnovationsAdmin@users.noreply.github.com>
2026-09-11 07:11:18 +0400

    test: provide global UUID seam for isolated payment tests

--- tests/support/wordpress-payment-runtime.php
+++ tests/support/wordpress-payment-runtime.php
@@ -119,6 +119,17 @@ function site_url($path = '', $scheme = null) {
     return 'https://example.test' . (string) $path;
 }
 
+if (!function_exists('wp_generate_uuid4')) {
+    function wp_generate_uuid4() {
+        $next = isset($GLOBALS['supcheckout_test_payment_runtime_uuid_sequence'])
+            ? (int) $GLOBALS['supcheckout_test_payment_runtime_uuid_sequence'] + 1
+            : 1;
+        $GLOBALS['supcheckout_test_payment_runtime_uuid_sequence'] = $next;
+
+        return sprintf('00000000-0000-4000-8000-%012x', $next);
+    }
+}
+
 if (!function_exists('wp_parse_url')) {
     function wp_parse_url($url, $component = -1) {
         return parse_url((string) $url, $component);
